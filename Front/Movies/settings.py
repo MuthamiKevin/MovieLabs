0f5fedec7fd5f106fd536cdfd6b4fcb908b8ff43
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-29 21:58:43 +0300

    add

--- Front/Movies/settings.py
+++ Front/Movies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w$o%j=+fw_5$njq$(b%e(s!g@%)ame!5wi+@ke0_ym(ck^&j1f'
+SECRET_KEY = 'django-insecure-sv=h%vo43l50ch6bd#epdlt-o0@-e#kl9&qds8jk00d^pt2el#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
